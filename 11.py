79be6c9ed01cecb21d74057d59e4b49d5e8992c9
Doron Shafrir <doron.abc@gmail.com>
2023-12-11 11:05:42 +0200

    p11 speed gain  by stacking both marginals

--- 11.py
+++ 11.py
@@ -1,10 +1,11 @@
 import numpy as np
-def total_dist(proj,expansion=2):
-    cumsum=proj.cumsum()
-    cumsum2=proj.sum()-cumsum
+def total_dist(A, expansion):
+    proj = np.array([A.sum(d) for d in (0,1)])
+    cumsum=proj.cumsum(1)
+    cumsum2=cumsum[0,-1]-cumsum
     factor=np.where(proj,1,expansion)
     return (factor*cumsum*cumsum2).sum()
 
 A=np.array([[*x] for x in open("11.txt").read().splitlines()])=="#"
-print("A part:", total_dist(A.sum(0))+total_dist(A.sum(1)))
-print("B part:", total_dist(A.sum(0),1e6)+total_dist(A.sum(1),1e6))
+for exp in (2,1_000_000):
+    print(total_dist(A,exp))
