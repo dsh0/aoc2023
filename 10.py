38ab448043bc56222cda0837b836d91681a7ecd6
Doron Shafrir <doron.abc@gmail.com>
2023-12-13 17:19:07 +0200

    day 10 added

--- 10.py
+++ 10.py
@@ -1,5 +1,12 @@
-import numpy as np
-shapes={"F":(1,1),"J":(-1,-1),"7":(1,-1),"L":(-1,1)}
-A=np.array([[*x] for x in open("10.txt").read().split()])
-x,y=(t[0] for t in np.where(A=='S'))
-print(f"{x=},{y=}")
+d={"F":(1,1),"J":(-1,-1),"7":(1,-1),"L":(-1,1),"|":(0,0),"-":(0,0)}
+A=open("10.txt").read().split()
+x,y,dx,dy,ii,area=19,88,1,0,0,0 # starting point & direction chosen manually
+try:
+    while True:
+        ii+=1
+        area+=y*dx
+        x,y=x+dx,y+dy
+        sx,sy=d[A[x][y]]
+        dx,dy=sx+dx,sy+dy
+except: # key 'S' not found in d, loop completed
+    print(ii//2, abs(area)-ii//2+1)
