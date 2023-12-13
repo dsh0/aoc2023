a4b393b65d2773e08b80a79aa703f1c8b8d05fc4
Doron Shafrir <doron.abc@gmail.com>
2023-12-13 13:49:25 +0200

    day 13: both part brute-force with numpy

--- 13.py
+++ 13.py
@@ -1,18 +1,11 @@
-from collections import defaultdict
-from itertools import combinations
+import numpy as np
 total = 0
 for block in open("13.txt").read().split('\n\n'):
-    A = block.split()
-    AT = [''.join(y) for y in zip(*A)] # A transposed
-    for M,factor in ((A,100),(AT,1)):
-        collisions = defaultdict(list) # collisions['..#.##']==[1,3,4] means rows 1,3,4 are all '..#.##' 
-        for ii,row in enumerate(M):
-            collisions[row].append(ii)
-        sums = defaultdict(lambda: 0) # sum[x] will be the number of identical rows ii,jj with ii==x-1-jj
-        for indices_with_same_row in collisions.values():
-            for ii,jj in combinations(indices_with_same_row,2):
-                sums[ii+jj+1]+=1
-        for sum, count in sums.items():
-            if 2*count==min(sum,2*len(M)-sum):
-                total+=factor*sum
-print(total//2)
+    A = np.array([[*row] for row in block.split()])
+    for M,factor in ((A,100),(A.transpose(),1)):
+        l = M.shape[0]
+        for ii in range(1,l):
+            B = M[:2*ii] if 2*ii<l else M[2*(ii-l):]
+            if (B!=B[::-1]).sum()==2: # 0 for part A, 2 for part B
+                total+=factor*ii
+print(total)
