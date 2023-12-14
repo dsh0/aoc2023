8b3006fa871f77f125adef12db67a707e404cdf0
Doron Shafrir <doron.abc@gmail.com>
2023-12-14 23:00:20 +0200

    days 10&12 minor changes

--- 10.py
+++ 10.py
@@ -1,12 +1,10 @@
-d={"F":(1,1),"J":(-1,-1),"7":(1,-1),"L":(-1,1),"|":(0,0),"-":(0,0)}
+d={"F":(1,1),"J":(-1,-1),"7":(1,-1),"L":(-1,1),"|":(0,0),"-":(0,0),"S":(2,2)}
 A=open("10.txt").read().split()
 x,y,dx,dy,ii,area=19,88,1,0,0,0 # starting point & direction chosen manually
-try:
-    while True:
-        ii+=1
-        area+=y*dx
-        x,y=x+dx,y+dy
-        sx,sy=d[A[x][y]]
-        dx,dy=sx+dx,sy+dy
-except: # key 'S' not found in d, loop completed
-    print(ii//2, abs(area)-ii//2+1)
+while dx*dy==0:
+    ii+=1
+    area+=y*dx
+    x,y=x+dx,y+dy
+    sx,sy=d[A[x][y]]
+    dx,dy=sx+dx,sy+dy
+print(ii//2, abs(area)-ii//2+1)
--- 12.py
+++ 12.py
@@ -6,22 +6,28 @@ import numpy as np
 #  (2) line 25 - the last character is '#' - in this case it must end with '.#####' (with seq[jj-1] '#'s, 5 is just an example), and before that it has '#'-sequence seq[::jj-1]
 
 ls=open("12.txt").read().splitlines()
-res=0
-for l in ls:
-    pat,seq=l.split()
-    pat="?".join([pat]*5) # part B only
-    seq=np.fromstring(seq,dtype=int,sep=',')
-    seq=np.tile(seq,5) # part B only
-    vec=np.zeros((len(pat)+1, len(seq)+1),dtype=int)
-    vec[0,0]=1
-    if '.' not in pat[:seq[0]]:
-        vec[seq[0],1]=1 # this is a special case which line 26 doesn't address since there's no '.' before the first block of '#'s
-    for ii,c in enumerate(pat):
-        # in the ii'th iteration we compute vec[ii+1,:]
-        if c!='#': 
-            vec[ii+1]+=vec[ii] #  counts solutions where pat[ii]=='.' after assignment of '?'s
-        for jj,l in enumerate(seq):
-            if ii>=l and '.' not in pat[ii+1-l:ii+1] and pat[ii-l]!='#': # if last l symbols can be '#':
-                vec[ii+1,jj+1]+=vec[ii-l,jj] #  counts solutions where pat[ii-l]=='.' && all(pat[ii-l+1:ii+1]=='#')
-    res+=vec[-1,-1]
-print(res)
+def solve(ls):
+    res=0
+    for l in ls:
+        pat,seq=l.split()
+        pat="?".join([pat]*5) # part B only
+        seq=np.fromstring(seq,dtype=int,sep=',')
+        seq=np.tile(seq,5) # part B only
+        vec=np.zeros((len(pat)+1, len(seq)+1),dtype=int)
+        vec[0,0]=1
+        if '.' not in pat[:seq[0]]:
+            vec[seq[0],1]=1 # this is a special case which line 26 doesn't address since there's no '.' before the first block of '#'s
+        suff = 0
+        for ii,c in enumerate(pat):
+            suff+=1
+            suff*= c!='.'
+            # in the ii'th iteration we compute vec[ii+1,:]
+            if c!='#': 
+                vec[ii+1]+=vec[ii] #  counts solutions where pat[ii]=='.' after assignment of '?'s
+            # TODO: vectorize with numpy the following loop
+            for jj,l in enumerate(seq):
+                if l<=suff and pat[ii-l]!='#': # if last l symbols can be '#':
+                    vec[ii+1,jj+1]+=vec[ii-l,jj] #  counts solutions where pat[ii-l]=='.' && all(pat[ii-l+1:ii+1]=='#')
+        res+=vec[-1,-1]
+    return res
+print(solve(ls))
